7fcf465629c8702844daff28b4984b848bb01d92
Byron Adams <byrron.adams54@gmail.com>
2012-11-10 00:13:28 +1300

    Added "Do this automatically" feature to page translations

--- data/translate-page.js
+++ data/translate-page.js
@@ -3,15 +3,19 @@ self.port.on('init', function (options) {
     var cancel = document.getElementById('cancel');
     var select_from = document.getElementById('from_lang');
     var select_to = document.getElementById('to_lang');
+    var auto = document.getElementById('auto');
 
     $('select').html('')
     $('#from_lang').append('<option class="flat" value="">'+options.locales['Auto_Detect']+'</option>');
     $('select').append(options.languages);
 
+
     button.onclick = function (evt) {
+
         self.port.emit('translate-page', {
             from : select_from.value,
-            to : select_to.value
+            to : select_to.value,
+            auto : auto.checked
         });
     };
 
