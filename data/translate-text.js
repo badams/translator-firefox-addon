5d8bbbd8e16da7492aae738b558b261d892d1cb3
Byron Adams <byron.adams54@gmail.com>
2012-12-29 18:31:07 +1300

    Added Syncing of Favorite languages to text translation panel, also Error handling among other polish

--- data/translate-text.js
+++ data/translate-text.js
@@ -10,7 +10,7 @@ self.port.on('init', function (options) {
     translator = new TabbedTranslator($('#translator'), {
         text : options.text,
         languages : options.languages,
-        tabs : ['en', 'fr', 'de'],
+        tabs : options.favorites,
         l10n : options.locales
     });
 
@@ -18,21 +18,18 @@ self.port.on('init', function (options) {
 
 
 self.port.on('translation-complete', function (data) {
-    if (data.result) {
+    if (data && data.result) {
         translator.setTab(data.to, data.result);
-        //translated_text = data.result;
     } else {
-        //translated_text = 'Error:'+ data.status
-        //handle error
+        translator.errorTab(data.to);
     }
 });
 
 self.port.on('detect-complete', function (data) {
-    if (data.language) {
+    if (data && data.language) {
         translator.setOriginLanguage(data.language);
-        //translated_text = data.result;
     } else {
-        //translated_text = 'Error:'+ data.status
+        translator.errorTab('origin');
     }
 });
 
@@ -41,8 +38,10 @@ var TabbedTranslator = function (el, options) {
     this.container = el;
     this.original_text = options.text;
     this.languages = options.languages;
-    this.lang = 'Unknown';
+    this.favs = options.tabs;
     this.l10n = options.l10n;
+    this.lang = this.l10n['Unknown'];
+    this.tab_count = 0;
 
     this.container.append('<ul class="tabs"></ul>');
     this.container.append('<div class="tab-content"></div>');
@@ -50,27 +49,13 @@ var TabbedTranslator = function (el, options) {
     this.tabs = this.container.find('ul.tabs');
     this.tab_content = this.container.find('div.tab-content');
 
-    this.tabs.append('<li class="add-tab"><i class="icon icon-add"></i>'+this.template('languageList')+'</li>');
-    this.addTab('origin').setTab('origin', this.original_text);
-
-    this.langSelect = this.tabs.find('div.lang-select');
-
-    if (options.tabs instanceof Array) {
-        for (var t = 0; t < options.tabs.length; t++) {
-            this.addTab(options.tabs[t]);
-        }
-    }
-
-    this.showTab('origin');
+    this.addTab('origin').setTab('origin', this.original_text).showTab('origin');
     this.getOriginLanguage();
 
     this.tabs.click($.proxy(this.clickTab, this));
-    this.langSelect.click($.proxy(this.selectLang, this));
 
     this.container.click($.proxy(function () {
-        if (this.langSelect.is(':visible')) {
-            this.langSelect.hide();
-        }
+        if (this.langSelect.is(':visible')) this.langSelect.hide();
     }, this));
 
     this.tab_content.find('div[data-lang="origin"] textarea').change(function () {
@@ -82,7 +67,7 @@ var TabbedTranslator = function (el, options) {
 
 
 TabbedTranslator.prototype = {
-
+    /* {{{ Templates */
     tpl : {
 
         tab : function (lang) {
@@ -91,6 +76,14 @@ TabbedTranslator.prototype = {
             content += '</li>';
 
             return content;
+        },
+
+        addTab : function () {
+            return '<li class="add-tab"><i class="icon icon-add"></i>'+this.template('languageList')+'</li>';
+        },
+
+        removeTab : function () {
+            return '<li class="remove-tab disabled"><i class="icon icon-remove"></i></li>';
         },
 
         tabContent : function (lang) {
@@ -102,9 +95,7 @@ TabbedTranslator.prototype = {
                 content += this.l10n['From'] + ': <span class="from">'+this.lang+'</span> '
                 content += this.l10n['To'] + ': <span class="to">'+this.languages[lang]+'</span>';
             } 
-            content += '</div><div class="spinner"></div>';
-            content += '</div>';
-            content += '</div>';
+            content += '</div></div></div>';
 
             return content;
         },
@@ -118,11 +109,11 @@ TabbedTranslator.prototype = {
             return html;
         },
     },
-
+    /* }}} */
+    /* {{{ Utils */
     template : function (tpl, vars) {
         return $.proxy(this.tpl[tpl], this)(vars);
     },
-
     
     translate : function (lang, text) {
         this.tab_content.find('div[data-lang="'+lang+'"] textarea').attr('disabled', true).parent().addClass('working');
@@ -135,40 +126,76 @@ TabbedTranslator.prototype = {
 
     getOriginLanguage : function () {
         this.tab_content.find('div[data-lang="origin"]').addClass('working');
-        self.port.emit('detect', this.original_text.substr(0, 180));
+        self.port.emit('detect', this.original_text.substr(0, 50));
     },
 
     setOriginLanguage : function (lang) {
         this.lang = this.languages[lang];
         this.tabs.find('li[data-lang="origin"] i').addClass(lang);
-        this.removeTab(lang);
         this.tab_content.find('div[data-lang="origin"]').removeClass('working');
+        this.tabs.append(this.template('addTab'));
+        this.tabs.append(this.template('removeTab'));
+        this.langSelect = this.tabs.find('div.lang-select');
         this.tab_content.find('div.lang-info span.from').html(this.languages[lang]);
+
+        for (var t in this.favs) this.addTab(t);
     }, 
 
-    addTab : function (lang) {
-        if (this.tabs.find('li[data-lang='+lang+']').get(0)) {
-            return this.showTab(lang);
-        }
-
-        this.tabs.find('.add-tab').before(this.template('tab', lang));
-        this.tab_content.append(this.template('tabContent', lang));
-
-        return this;
+    getText : function () {
+        return this.tab_content.find('div[data-lang="origin"] textarea').val();
     },
 
+    saveFavorites : function () {
+        var favs = [];
+        for (var f in this.favs) favs.push(f)
+        self.port.emit('save-favorites', favs);
+    },
+    /* }}} */
+    /* {{{ Tab API */
+    /* {{{ addTab(lang) */
+    addTab : function (lang) {
+        if (this.tabs.find('li[data-lang='+lang+']').get(0))
+            return this.showTab(lang);
+
+        if (this.lang !== this.languages[lang]) {
+            if (this.tabs.find('.add-tab')[0])
+                this.tabs.find('.add-tab').before(this.template('tab', lang));
+            else
+                this.tabs.append(this.template('tab', lang));
+
+            this.tab_content.append(this.template('tabContent', lang));
+            
+            this.tab_count++;
+
+            if (this.tab_count  > 7) {
+                this.tabs.find('li.add-tab').hide();
+            }
+
+            this.favs[lang] = this.languages[lang];
+        }
+        return this;
+    },
+    /* }}} */
+    /* {{{ removeTab (lang) */
     removeTab : function (lang) {
         var tab =  this.tabs.find('li[data-lang="'+lang+'"]');
-        if (tab.is('active')) {
-            this.showTab('origin');
+        if (tab.is('.active')) {
+            this.showTab(tab.prev().attr('data-lang') || 'origin');
         }
 
         tab.remove();
         this.tab_content.find('div[data-lang="'+lang+'"]').remove();
+        this.tab_count--;
+        this.saveFavorites();
         
+        if (this.tab_count <= 7) this.tabs.find('li.add-tab').show();
+        
+        delete this.favs[lang];
+
         return this;
     },
-
+    /* }}} */
+    /* {{{ showTab(lang) */
     showTab : function (lang) {
         this.tabs.find('li.active').removeClass('active');
         this.tabs.find('li[data-lang="'+lang+'"]').addClass('active');
@@ -178,28 +205,43 @@ TabbedTranslator.prototype = {
     
 
         if ('origin' !== lang) {
+            this.tabs.find('li.remove-tab').removeClass('disabled');
             var last_translate = parseInt(this.tab_content.find('div[data-lang="'+lang+'"] textarea').attr('data-edited')) || 0;
             var last_edit = parseInt(this.tab_content.find('div[data-lang="origin"] textarea').attr('data-edited'));
 
             if (last_edit > last_translate) {
                 this.translate(lang, this.getText());
             }
+        } else {
+            this.tabs.find('li.remove-tab').addClass('disabled');
         }
 
         return this;
     },
-
+    /* }}} */
+    /* {{{ setTab(lang, content)*/
     setTab : function (lang, data) {
         this.tab_content.find('div[data-lang="'+lang+'"] textarea')
             .val(data)
             .attr('data-edited', new Date().getTime())
             .attr('disabled', false)
             .parent().removeClass('working');
-    },
 
+        return this;
+    },
+    /* }}} */
+    /* {{{ errorTab(lang) */
+    errorTab : function (lang) {
+        this.tabs.find('li[data-lang="'+lang+'"] i').addClass('icon icon-error');
+        this.tab_content.find('div[data-lang="'+lang+'"]').addClass('error');
+        this.setTab(lang, this.l10n['Translation_Error']);
+    },
+    /* }}} */
+    /* }}} */
+    /* {{{ Events */
     clickTab : function (e) {
         var $t = $(e.target);
-        $t = $t.is('li > i, li > a') ?  $t.parents('li') : $t;
+        $t = $t.is('i') ?  $t.parent() : $t;
 
         if ($t.is('ul.tabs > li[data-lang]')) {
             var lang = $t.attr('data-lang');
@@ -207,24 +249,31 @@ TabbedTranslator.prototype = {
         }
 
         if ($t.is('li.add-tab')) {
-            this.langSelect.show();
+            var left  = parseInt(this.langSelect.toggle().css('left').replace('px', ''));
+
+            // Stop the lang-select from going past the edge of the viewport.
+            if (left < 130) 
+                this.langSelect.css('left', 130);
+            else 
+                this.langSelect.css('left', 'auto');
+
             return false;
         }
-    },
 
-    getText : function () {
-        return this.tab_content.find('div[data-lang="origin"] textarea').val();
-    },
-
+        if ($t.is('li.remove-tab:not(.disabled)')) {
+            this.removeTab(this.tabs.find('li.active').attr('data-lang'));
+            this.saveFavorites();
+        }
     
-    selectLang : function (e) {
-        var $t = ('I' == e.target.nodeName) ? $(e.target.parentNode) : $(e.target);
-
         if ($t.is('div.lang-select > div[data-lang]')) {
             var lang = $t.attr('data-lang');
             this.langSelect.hide();
-            this.addTab(lang).showTab(lang);
+
+            if (this.lang !== this.languages[lang]) 
+                this.addTab(lang).showTab(lang);
+                this.saveFavorites();
         }
-    }
+    },
+    /* }}} */
 };
 
