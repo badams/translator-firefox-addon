68178c7c7bdecd531abc8fffb5da4065e5c87bd0
Byron Adams <byron.adams54@gmail.com>
2013-01-10 01:22:28 +1300

    Initial commit for new Favorites Manager interface

--- data/favorite-languages.js
+++ data/favorite-languages.js
@@ -1,47 +1,167 @@
+var Manager;
+
 self.port.on('init', function (options) {
-
-    var $favorites = $('#favorites_select'),
-        $available = $('#available_select');
-
-    $('select').html('');
-
-    for (var f in options.favorites) {
-        $('#favorites_select').append('<option class="flag '+f+'" value='+ f +'>'+options.favorites[f]+'</option>');
-    }
-
-    for (var l in options.languages) {
-        if (!options.favorites[l]) {
-            $('#available_select').append('<option class="flag '+l+'" value='+ l +'>'+options.languages[l]+'</option>');
-        }
-    }
-
-    $('#add').click(function () {
-        var selected = $available.val();
-
-        if (selected) {
-            $available.find('option[value='+selected+']').appendTo($favorites);
-        }
-    });
-
-    $('#remove').click(function () {
-        var selected = $favorites.val();
-
-        if (selected) {
-            $favorites.find('option[value='+selected+']').appendTo($available);
-        }
-    });
-
-    $('#save').click(function () {
-        var new_favs = [];
-        $favorites.find('option').each(function () {
-            new_favs.push(this.value);
-        });
-
-        self.port.emit('save', new_favs);
-    });
-
-    $('#close').click(function () {
-        self.port.emit('close');
-    });
+    Manager = new LanguageManager(document.getElementById('LangManager'), options); 
 });
 
+/* {{{ LanguageManager */
+/* {{{ Constructor */
+var LanguageManager = function (el, options) {
+    this.container = el;
+    this.languages = options.languages;
+    this.favorites = options.favorites;
+    this.tts_langs = options.tts_langs;
+
+    this.list = this.container.querySelector('div.favorite-languages');
+    this.toolbar = this.container.querySelector('div.language-toolbar');
+    this.langSelect = this.toolbar.querySelector('div.lang-select');
+
+    this.list.innerHTML = '';
+    this.langSelect.innerHTML = '';
+
+    this.createLangSelect();
+
+    for (var f in this.favorites) this.addItem(f);
+
+    var that = this;
+    this.container.onclick =  function (e) {
+        that.click.apply(that, [e]);
+    };
+
+    return this;
+};
+/* }}} */
+/* {{{ Api */
+LanguageManager.prototype = {
+    /* {{{ save() */
+    /**
+     * Saves the current list of languages to favorites
+     */
+    save : function () {
+        var items = this.list.children, data = [];
+        for (var i = 0; i < items.length; i++) 
+            data.push(items[i].getAttribute('data-lang'));
+
+        self.port.emit('save', data);
+        self.port.emit('close');
+    },
+    /* }}} */
+    /* {{{ getItem(lang) */
+    /**
+     * returns an item from the list of favorite languages.
+     * 
+     * @param lang (language code)
+     * @return DOM Element
+     */
+    getItem : function (lang) {
+        return this.list.querySelector('div[data-lang="'+lang+'"]') || false;
+    },
+    /* }}} */
+    /* {{{  addItem(lang) */
+    /**
+     * Adds a new Item to the favorites list based on the given language code.
+     * @param lang (language code)
+     */
+    addItem : function (lang) {
+        if (!this.getItem(lang)) {
+            this.list.innerHTML += this.createItem(lang);
+        }
+    },
+    /* }}} */
+    /* {{{  removeItem(lang) */
+    /**
+     * Removes an item from the list.
+     * @param lang (language code)
+     */
+    removeItem : function (lang) {
+        this.list.removeChild(this.getItem(lang));
+    },
+    /* }}} */
+    /* {{{ createItem(lang) */
+    /**
+     * Generates the html for a item in the favorites list, 
+     * based on the given language code.
+     *
+     * @param lang (language code)
+     */
+    createItem : function (lang) {
+        var html = '<div data-lang="'+lang+'" class="language-item">';
+        
+        html += '<span class="item-title flag '+lang+'">'+this.languages[lang]+' ('+lang.toUpperCase()+')</span>';
+        html += '<div class="controls">';
+
+        if (-1 < this.tts_langs.indexOf(lang)) {
+            html += '<i class="icon icon-sound"></i>';
+        }
+        
+        html += '<button class="btn remove"><i class="icon icon-remove"></i></button></div>';
+        html += '</div>';
+
+        return html;
+    },
+    /* }}} */
+    /* {{{ click */
+    /**
+     * Click handler for enire widget, 
+     * delegates actions based on what element was clicked.
+     * 
+     * @arg (Event Object)
+     */
+    click : function (e) {
+        if ('btn remove' === e.target.className) {
+            var lang = e.target.parentNode.parentNode.getAttribute('data-lang');
+            this.removeItem(lang);
+        }
+
+        if ('btn add' === e.target.className) {
+            this.toggleLangSelect();
+            return false;
+        }
+
+        if ('new-lang' === e.target.className) {
+            this.addItem(e.target.getAttribute('data-lang'));
+            this.toggleLangSelect();
+            return false;
+        }
+
+        if ('block' === this.langSelect.style.display) {
+            this.langSelect.style.display = 'none';
+            return false;
+        }
+
+        if ('save' === e.target.getAttribute('id')) {
+            this.save();
+            return false;
+        }
+
+        if ('close' === e.target.getAttribute('id')) {
+            self.port.emit('close');
+            return false;
+        }
+    },
+    /* }}} */
+    /* {{{ createLangSelect() */
+    /** 
+     *  Generates the html for the drop-down language selector.
+     */
+    createLangSelect : function () {
+        var html = '';
+        for (var lang in this.languages) {
+            html += '<div class="new-lang" data-lang="'+lang+'"><i class="flag '+lang+'"></i>'+this.languages[lang]+'</div>';
+        }
+
+        this.langSelect.innerHTML = html;
+    },
+    /* }}} */
+    /* {{{ toggleLangSelect() */
+    /** 
+     * toggles the visiblity of the drop-down
+     * language selector.
+     */
+    toggleLangSelect : function () {
+        this.langSelect.style.display =  ('block' === this.langSelect.style.display) ? 'none' : 'block';
+    },
+    /* }}} */
+};
+/* }}} */
+/* }}} */
